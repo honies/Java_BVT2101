a48672b58d6994dbcb848d5b579979e0c0f60c87
honies <90001450+honies@users.noreply.github.com>
2022-11-18 00:41:31 +0300

    task2 completed

--- tasks/task2/src/task2.java
+++ tasks/task2/src/task2.java
@@ -1,19 +1,66 @@
-import java.math.BigDecimal;
 import java.util.Arrays;
 
 public class task2 {
     public static void main(String[] args) {
+        System.out.println("------№1------");
         System.out.println(repeat("Hello", 3));
+        System.out.println(repeat("Nice", 5));
+        System.out.println(repeat("Stop", 1));
+
+
+        System.out.println("------№2------");
         System.out.println(differenceMaxMin(new int[]{1,3,54,6,7,13}));
-        System.out.println(isAvgWhole(new int[]{2,4,5,6,7,8,8}));
-        System.out.println(cumulativeSum(new int[]{1,2,4,5}));
-        System.out.println((int)getDecimalPlaces("43.222"));
+        System.out.println(differenceMaxMin(new int[]{15,3,5,24,-6,-10,0}));
+        System.out.println(differenceMaxMin(new int[]{100,-99,2,457,-200}));
+
+
+        System.out.println("------№3------");
+        System.out.println(isAvgWhole(new int[]{1,3}));
+        System.out.println(isAvgWhole(new int[]{5,1,3,5,6,10}));
+        System.out.println(isAvgWhole(new int[]{14,34,2,1}));
+
+
+        System.out.println("------№4------");
+        System.out.println(Arrays.toString(cumulativeSum(new int[]{1,2,4,5})));
+        System.out.println(Arrays.toString(cumulativeSum(new int[]{1,2,12,45})));
+        System.out.println(Arrays.toString(cumulativeSum(new int[]{1,4,12,20})));
+
+
+        System.out.println("------№5------");
+        System.out.println((int)getDecimalPlaces("43.20"));
+        System.out.println((int)getDecimalPlaces("23.222"));
+        System.out.println((int)getDecimalPlaces("100"));
+
+
+        System.out.println("------№6------");
+        System.out.println(fibonacci(2));
+        System.out.println(fibonacci(5));
         System.out.println(fibonacci(10));
+
+
+        System.out.println("------№7------");
         System.out.println(isValid("134 43342"));
+        System.out.println(isValid("853a7"));
+        System.out.println(isValid("13442"));
+
+
+        System.out.println("------№8------");
         System.out.println(isStrangePair("mother", "rum"));
+        System.out.println(isStrangePair("bush", "hubris"));
+        System.out.println(isStrangePair("", ""));
+
+
+        System.out.println("------№9------");
         System.out.println(isPrefix("automation", "auto-"));
+        System.out.println(isPrefix("retrospect", "sub-"));
+
         System.out.println(isSuffix("arachnophobia", "-phobia"));
+        System.out.println(isSuffix("vocation", "-logy"));
+
+        System.out.println("------№10------");
         System.out.println(boxSeq(1));
+        System.out.println(boxSeq(4));
+        System.out.println(boxSeq(9));
     }
 
     public static String repeat(String string, int num){
@@ -56,16 +103,30 @@ public class task2 {
         }
     }
 
-    public static String cumulativeSum(int[] list){
-        for(int n = 1; n == list[list.length - 1]; n++){
-            list[n] = list[n-1] + list[n];
+    public static int[] cumulativeSum(int[] list){
+        int[] newList = new int[list.length];
+        int a = 0;
+
+        for (int i = 0; i < list.length; i++) {
+            a += list[i];
+            newList[i] = a;
         }
-        return Arrays.toString(list);
+        return newList;
     }
 
-    public static double getDecimalPlaces(String string){
-        double value = Double.parseDouble(string);
-        return (int)BigDecimal.valueOf(value).scale();
+    public static int getDecimalPlaces(String string){
+        char[] charList = string.toCharArray();
+        int c = 0;
+        for (int i = 0; i < charList.length; i++) {
+            if (charList[i] == ".".charAt(0)) {
+                c = i;
+                break;
+            }
+        }
+        if (c == 0) {
+            return 0;
+        }
+        else return charList.length - (c + 1);
     }
 
     public static int fibonacci(int num){
