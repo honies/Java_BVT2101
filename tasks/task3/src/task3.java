2aee28de68f58d470ead1a94ef996ab4158834b7
honies <90001450+honies@users.noreply.github.com>
2022-11-21 04:16:15 +0300

    new methods

--- tasks/task3/src/task3.java
+++ tasks/task3/src/task3.java
@@ -15,15 +15,15 @@ public class task3 {
 
 
         System.out.println("------№3------");
-        System.out.println();
-        System.out.println();
-        System.out.println();
+        System.out.println(checkPerfect(6));
+        System.out.println(checkPerfect(496));
+        System.out.println(checkPerfect(97));
 
 
         System.out.println("------№4------");
-        System.out.println();
-        System.out.println();
-        System.out.println();
+        System.out.println(flipEndChars("Cat, dog, and mouse."));
+        System.out.println(flipEndChars("ada"));
+        System.out.println(flipEndChars("z"));
 
 
         System.out.println("------№5------");
@@ -72,9 +72,30 @@ public class task3 {
 
     public static boolean checkPerfect(int n){
         int divSum = 0;
-        for(int i = 0; i < n; i++){
-            
+        for(int i = 1; i < n; i++){
+            if(n % i == 0){
+                divSum += i;
+            }
         }
+        return n == divSum;
+    }
+
+    public static String flipEndChars(String string){
+        if(string.length() < 2){
+            return "Incompatible.";
+        }else{
+            char[] flipString = string.toCharArray();
+            if(flipString[0] == flipString[flipString.length - 1]){
+                return "Two's a pair.";
+            }else{
+                char firstChar = flipString[0];
+                flipString[0] = flipString[flipString.length - 1];
+                flipString[flipString.length - 1] = firstChar;
+                String newString = new String(flipString);
+                return newString;
+            }
+        }
+
     }
 
     public static String longestZero(String string){
